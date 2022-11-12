28d455eefe5e1f187a370f4cf9e70d0b2e279f22
ziwu <549863285@qq.com>
2022-11-12 16:37:48 +0800

    feat: 展厅评论功能

--- pages/showroom-detail/index.js
+++ pages/showroom-detail/index.js
@@ -9,15 +9,9 @@ Page({
     info: {},
     pages: [],
     loading: true,
-    indicatorDots: true,
-    vertical: false,
-    interval: 3000,
-    duration: 1000,
-    autoplay: false,
-    navigation: {
-      type: 'dots'
-    },
     currentPage: -1,
+    comments: [],
+    commentContent: ''
   },
 
   onLoad(options) {
@@ -28,6 +22,7 @@ Page({
       options
     }, () => {
       this.getShowroomDetail()
+      this.getComments();
     })
   },
 
@@ -43,17 +38,93 @@ Page({
 
   },
 
+  handleCommentChange(e) {
+    this.setData({
+      commentContent: e.detail.value
+    })
+  },
+
+  submitComment() {
+    const {
+      commentContent,
+      options
+    } = this.data;
+    const user = wx.getStorageSync('userInfo');
+    const _ = this;
+    if (!user || !user.id) {
+      wx.showToast({
+        title: '请先登录再评论',
+        duration: 1500,
+        icon: 'none'
+      })
+      setTimeout(() => {
+        wx.navigateTo({
+          url: '/pages/register/index',
+        })
+      }, 1500);
+      return;
+    }
+    if (!_.data.commentContent) {
+      return wx.showToast({
+        title: '评论内容不能位空~',
+        icon: 'none'
+      });
+    }
+    wx.request({
+      url: `${_.data.domain}api/comment/create`,
+      method: 'POST',
+      data: {
+        content: commentContent,
+        showroomId: options.id,
+        accountId: user.id
+      },
+      success(res) {
+        if (res.data.code === '0000') {
+          _.setData({
+            commentContent: ''
+          })
+          _.getComments();
+
+        } else {
+          wx.showToast({
+            title: res.data.msg,
+          })
+        }
+      }
+    })
+  },
+
+  getComments() {
+    const _ = this;
+    wx.request({
+      url: `${_.data.domain}api/comment/query`,
+      data: {
+        showroomId: _.data.options.id
+      },
+      success(res) {
+        if (res.data.code === '0000') {
+          const comments = res.data.data.map(c => {
+            const oriNickName = c.account.nickName;
+            const tn = oriNickName.substr(0, 3) + '****' + oriNickName.substr(-2);
+            c.account.nickName = tn;
+            c.account.phone = tn;
+            c.createAt = dayjs(c.createAt).format('MM.DD')
+            return c;
+          })
+          _.setData({
+            comments
+          })
+        }
+      }
+    })
+  },
+
   handlePageClick(e) {
     const index = e.currentTarget.dataset.index
     let currentPage = index;
 
     if (index === this.data.pages.length - 1) {
       currentPage = -1;
-      // setTimeout(() => {
-      //   this.setData({
-      //     currentPage
-      //   })
-      // }, 800);
     }
     this.setData({
       currentPage
